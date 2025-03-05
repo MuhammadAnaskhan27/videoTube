29d1109e818d90ef9b9f515f4bcaea890fff938f
M.Anas Khan <90131661+MuhammadAnaskhan27@users.noreply.github.com>
2025-03-05 18:05:28 +0500

    done with models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
     username: {
@@ -50,5 +50,15 @@ const userSchema = new Schema(
   },
   { timestamps: true } // Automatically add createdAt & updatedAt
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
